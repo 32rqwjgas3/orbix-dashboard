96466ff5b561f56e364be6e8c2e825497401dabc
32rqwjgas3 <yytclipgod@gmail.com>
2025-07-04 02:45:08 -0700

    Add detailed error logging to callback route

--- app.js
+++ app.js
@@ -37,6 +37,41 @@ app.get('/callback', async (req, res) => {
   if (!code) return res.send('No code provided');
 
   try {
+    const tokenResponse = await axios.post(
+      'https://discord.com/api/oauth2/token',
+      new URLSearchParams({
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        grant_type: 'authorization_code',
+        code,
+        redirect_uri: REDIRECT_URI,
+        scope: 'identify',
+      }),
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      }
+    );
+
+    const userResponse = await axios.get('https://discord.com/api/users/@me', {
+      headers: {
+        Authorization: `Bearer ${tokenResponse.data.access_token}`,
+      },
+    });
+
+    const user = userResponse.data;
+    res.send(`
+      <h1>Welcome, ${user.username}#${user.discriminator}</h1>
+      <img src="https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png" width="100" />
+    `);
+  } catch (err) {
+    console.error("OAuth2 Error:", err.response?.data || err.message);
+    res.send(`<pre>Authentication failed.\n\n${JSON.stringify(err.response?.data || err.message, null, 2)}</pre>`);
+  }
+});
+
+  try {
     // Exchange code for access token
     const tokenResponse = await axios.post(
       'https://discord.com/api/oauth2/token',
